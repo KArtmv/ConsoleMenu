d88617eaf93821de6c4371e2858b33fe2b690eeb
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-10-28 18:10:47 +0200

    Refactor -Add ReadResourceFile class, which inject by constructor -Add method uses ReadResourceFile to read source data by file path

--- src/main/java/ua/foxminded/javaspring/consoleMenu/data/generator/sourceData/ResourcesFilesDatabaseData.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/data/generator/sourceData/ResourcesFilesDatabaseData.java
@@ -1,36 +1,51 @@
 package ua.foxminded.javaspring.consoleMenu.data.generator.sourceData;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import ua.foxminded.javaspring.consoleMenu.data.ReadResourcesFile;
+
+import java.util.List;
 
 @Component
 public class ResourcesFilesDatabaseData {
 
-    @Value("${databaseResourseFilepath.GROUPS_FILE}")
+    private ReadResourcesFile readFile;
+
+    @Autowired
+    public ResourcesFilesDatabaseData(ReadResourcesFile readFile) {
+        this.readFile = readFile;
+    }
+
+    @Value("${databaseResourceFilepath.GROUPS_FILE}")
     private String GROUPS_FILE;
 
-    @Value("${databaseResourseFilepath.COURSES_FILE}")
+    @Value("${databaseResourceFilepath.COURSES_FILE}")
     private String COURSES_FILE;
 
-    @Value("${databaseResourseFilepath.FIRST_NAME_FILE}")
+    @Value("${databaseResourceFilepath.FIRST_NAME_FILE}")
     private String FIRST_NAME_FILE;
 
-    @Value("${databaseResourseFilepath.LAST_NAME_FILE}")
+    @Value("${databaseResourceFilepath.LAST_NAME_FILE}")
     private String LAST_NAME_FILE;
 
-    public String getGroupsFilePath() {
-        return GROUPS_FILE;
+    public List<String> getGroupsFilePath() {
+        return getResourceData(GROUPS_FILE);
     }
 
-    public String getCoursesFilePath() {
-        return COURSES_FILE;
+    public List<String> getCoursesFilePath() {
+        return getResourceData(COURSES_FILE);
     }
 
-    public String getFirstNameFilePath() {
-        return FIRST_NAME_FILE;
+    public List<String> getFirstNameFilePath() {
+        return getResourceData(FIRST_NAME_FILE);
     }
 
-    public String getLastNameFilePath() {
-        return LAST_NAME_FILE;
+    public List<String> getLastNameFilePath() {
+        return getResourceData(LAST_NAME_FILE);
+    }
+
+    private List<String> getResourceData(String filePath){
+        return readFile.getData(filePath);
     }
 }
