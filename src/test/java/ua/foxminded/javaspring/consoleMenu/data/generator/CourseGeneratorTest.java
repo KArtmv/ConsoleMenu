594a418c4b726b4bde5add29acb93abeebe757c1
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-10-29 14:00:41 +0100

    Add InitializeObject to initial a list of student instead creating new

--- src/test/java/ua/foxminded/javaspring/consoleMenu/data/generator/CourseGeneratorTest.java
+++ src/test/java/ua/foxminded/javaspring/consoleMenu/data/generator/CourseGeneratorTest.java
@@ -3,18 +3,20 @@ package ua.foxminded.javaspring.consoleMenu.data.generator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.*;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
-
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import ua.foxminded.javaspring.consoleMenu.data.generator.sourceData.ResourcesFilesDatabaseData;
 import ua.foxminded.javaspring.consoleMenu.model.Course;
+import ua.foxminded.javaspring.consoleMenu.pattern.InitializeObject;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CourseGeneratorTest {
@@ -32,12 +34,11 @@ public class CourseGeneratorTest {
 
     @Test
     void generate_shouldReturnListOfCourse_whenIsValidDataProvided() {
-        List<Course> expected = new ArrayList<>();
-        expected.add(new Course(1L, "test", "test"));
-        expected.add(new Course(2L, "test", "test"));
-        expected.add(new Course(3L, "test", "test"));
+        InitializeObject initializeObject = new InitializeObject();
+        List<Course> expected = initializeObject.coursesListInit();
 
-        when(resourcesFiles.getCourses()).thenReturn(Arrays.asList("test_test", "test_test", "test_test"));
+        when(resourcesFiles.getCourses()).thenReturn(Arrays.asList("courseName_courseDescription",
+                "courseName_courseDescription", "courseName_courseDescription"));
 
         List<Course> result = courseGenerator.generate();
 
