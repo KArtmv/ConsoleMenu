eda839193f1efd8d9ff653c8e9b84e1ab11649e5
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-11-05 13:26:32 +0100

    Refactor dao classes -Move repeated methods to DAO.class -Create DAO.class which contains those methods

--- src/main/java/ua/foxminded/javaspring/consoleMenu/dao/CourseDAO.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/dao/CourseDAO.java
@@ -1,15 +1,15 @@
-package ua.foxminded.javaspring.consoleMenu.dao;
-
-import ua.foxminded.javaspring.consoleMenu.model.Course;
-
-public interface CourseDAO {
-    boolean addCourse(Course course);
-
-    boolean isValidCourseID(Course course);
-
-    boolean isCourseTableExist(String sqlQuery);
-
-    void createCourseTable(String sqlQuery);
-
-    boolean isCourseTableEmpty();
-}
+//package ua.foxminded.javaspring.consoleMenu.dao;
+//
+//import ua.foxminded.javaspring.consoleMenu.model.Course;
+//
+//public interface CourseDAO<T> {
+////    boolean addCourse(Course course);
+//
+////    boolean isValidCourseID(Course course);
+//
+////    boolean isCourseTableExist(String sqlQuery);
+//
+////    void createCourseTable(String sqlQuery);
+//
+////    boolean isCourseTableEmpty();
+//}
