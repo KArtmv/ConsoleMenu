61c07dfc7c1e20187af21eccba33ef1a5335fea9
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-11-13 11:31:36 +0100

    Create class to allows console input -It can input Alphabetic, or Integer or menu option input

---
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/options/input/ConsoleInput.java
@@ -0,0 +1,52 @@
+package ua.foxminded.javaspring.consoleMenu.options.input;
+
+import java.util.Scanner;
+
+public class ConsoleInput {
+
+    private Scanner sc;
+    private static final String regexForAlphabetic = "[^a-zA-Z]+";
+    private static final String regexForInteger = "-?\\d+";
+    private static final String regexForMenu = "[^1-9x]";
+
+
+    public ConsoleInput() {
+        this.sc = new Scanner(System.in);
+    }
+
+    public Integer inputNumbers(){
+        int receivedNumber = 0;
+            try {
+                receivedNumber = Integer.parseInt(input(regexForInteger));
+            } catch (NumberFormatException e) {
+                System.out.println("Received is illegal argument. Try again.");
+            }
+        return receivedNumber;
+    }
+
+    public String inputCharacters(){
+        return input(regexForAlphabetic);
+    }
+
+    public String menuInput(){
+        return input(regexForMenu);
+    }
+
+    private String input(String regex){
+        String input;
+        do {
+            input = sc.nextLine();
+        } while(isValidInput(input, regex));
+        return input;
+    }
+
+    private boolean isValidInput(String input, String regex){
+        boolean isValid = false;
+        if (input == null || input.isEmpty() || input.matches("\\s+") || input.matches(regex)) {
+            isValid = true;
+
+            System.out.println("Received is illegal argument. Try again.");
+        }
+        return isValid;
+    }
+}
