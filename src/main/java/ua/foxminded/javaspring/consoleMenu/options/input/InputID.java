7e4fd33bfb8120898e0e2f16844ce38c98132945
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-11-13 12:01:08 +0100

    Create class to check existing received ID

---
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/options/input/InputID.java
@@ -0,0 +1,33 @@
+package ua.foxminded.javaspring.consoleMenu.options.input;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import ua.foxminded.javaspring.consoleMenu.dao.DAO;
+
+public class InputID<T> {
+
+    private ConsoleInput consoleInput;
+    private DAO<T> dao;
+
+    @Autowired
+    public InputID(ConsoleInput consoleInput, DAO<T> dao) {
+        this.consoleInput = consoleInput;
+        this.dao = dao;
+    }
+
+    public Long inputID(){
+        int receivedID;
+        do {
+            receivedID = consoleInput.inputNumbers();
+        } while (!isValidID(receivedID));
+        return (long) receivedID;
+    }
+
+    private boolean isValidID(Integer receivedID){
+        if (dao.isValidItemID(receivedID)){
+            return true;
+        } else {
+            System.out.println("Invalid selected ID, is not exist. Please try again.");
+            return false;
+        }
+    }
+}
