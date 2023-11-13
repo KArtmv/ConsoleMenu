9c866a1afff3da9c1ff2927857899e60f9209c2e
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-11-13 12:14:56 +0100

    Create class of view available groups

---
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/options/output/OutputListOfGroup.java
@@ -0,0 +1,31 @@
+package ua.foxminded.javaspring.consoleMenu.options.output;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import ua.foxminded.javaspring.consoleMenu.dao.GroupDAO;
+import ua.foxminded.javaspring.consoleMenu.model.Group;
+
+import java.util.List;
+
+public class OutputListOfGroup {
+
+    private GroupDAO groupDAO;
+
+    @Autowired
+    public OutputListOfGroup(GroupDAO groupDAO) {
+        this.groupDAO = groupDAO;
+    }
+
+    public void viewAllGroups(){
+        List<Group> groups = groupDAO.listOfItems();
+        if (!CollectionUtils.isEmpty(groups)){
+            output(groups);
+        }
+    }
+
+    private void output(List<Group> groups) {
+        for (Group group : groups) {
+            System.out.printf("ID: %d,  group name: %s.\n", group.getGroupID(), group.getGroupName());
+        }
+    }
+}
