20d98794c22c0278fd1ecc07618a98239253d6be
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-11-13 12:20:07 +0100

    Create class of output count of student studying on that groups with received count students or less

---
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/options/controller/groupOption/OutputStudentsAtGroupByCount.java
@@ -0,0 +1,40 @@
+package ua.foxminded.javaspring.consoleMenu.options.controller.groupOption;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import ua.foxminded.javaspring.consoleMenu.model.CounterStudentsAtGroup;
+import ua.foxminded.javaspring.consoleMenu.options.input.ConsoleInput;
+import ua.foxminded.javaspring.consoleMenu.service.GroupService;
+
+import java.util.List;
+
+public class OutputStudentsAtGroupByCount {
+
+    private GroupService groupService;
+    private ConsoleInput consoleInput;
+
+    @Autowired
+    public OutputStudentsAtGroupByCount(GroupService groupService, ConsoleInput consoleInput) {
+        this.groupService = groupService;
+        this.consoleInput = consoleInput;
+    }
+
+    public void groupsByCount(){
+        System.out.println("Input count of students at course.");
+        int receivedCountOfStudent = consoleInput.inputNumbers();
+        List<CounterStudentsAtGroup> studentsAtGroups = groupService.counterStudentsAtGroups(receivedCountOfStudent);
+
+        if (CollectionUtils.isEmpty(studentsAtGroups)){
+            System.out.printf("Could not find a group with %d or fewer students.\n", receivedCountOfStudent);
+        } else {
+            outputResult(studentsAtGroups);
+        }
+    }
+
+    private void outputResult(List<CounterStudentsAtGroup> studentsAtGroups){
+        for (CounterStudentsAtGroup studentsAtGroup : studentsAtGroups) {
+            System.out.printf("%d of students at group: %s.\n",
+                    studentsAtGroup.getStudentsCount(), studentsAtGroup.getGroupName());
+        }
+    }
+}
