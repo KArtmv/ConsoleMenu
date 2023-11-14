faee15a0316d1272b589b3e77563a03bd204327c
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-11-14 12:25:32 +0100

    Refactor -Remove unnecessary methods -Fix SQl querys -Change input argument of some methods

--- src/main/java/ua/foxminded/javaspring/consoleMenu/repository/CourseRepo.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/repository/CourseRepo.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import ua.foxminded.javaspring.consoleMenu.dao.CourseDAO;
 import ua.foxminded.javaspring.consoleMenu.dao.DAO;
-import ua.foxminded.javaspring.consoleMenu.data.tables.sqlScripts.SQLQueryOfCreateTable;
+import ua.foxminded.javaspring.consoleMenu.databaseInitializer.tables.sqlScripts.SQLQueryOfCreateTable;
 import ua.foxminded.javaspring.consoleMenu.model.Course;
+import ua.foxminded.javaspring.consoleMenu.model.Student;
 import ua.foxminded.javaspring.consoleMenu.rowmapper.CourseMapper;
 
 import java.sql.ResultSet;
@@ -14,15 +16,16 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class CourseRepo implements DAO<Course> {
+public class CourseRepo implements CourseDAO {
 
     private SQLQueryOfCreateTable queryOfCreateTable;
     private JdbcTemplate jdbcTemplate;
 
     private static final String SQL_ADD_NEW_COURSE = "insert into courses (course_name, course_description) values (?, ?)";
-    private static final String SQL_CHECK_IS_COURSE_EXIST = "select course_id from course where course_id=?";
+    private static final String SQL_CHECK_IS_COURSE_ID_EXIST = "select course_id from courses where course_id=?";
     private static final String SQL_CHECK_IS_COURSE_TABLE_EMPTY = "SELECT COUNT(*) FROM courses";
-    private static final String SQL_GET_COURSE_BY_ID = "select * from course where course_id=?";
+    private static final String SQL_GET_COURSE_BY_ID = "select * from courses where course_id=?";
+    private static final String SQL_GET_LIST_OF_COURSE = "select * from courses";
 
     @Value("${sqlQuery.IsTableExist.SQL_CHECK_IS_TABLE_EXIST}")
     private String SQL_CHECK_IS_TABLE_EXIST;
@@ -36,8 +39,8 @@ public class CourseRepo implements DAO<Course> {
     }
 
     @Override
-    public boolean isValidItemID(Course course) {
-        return jdbcTemplate.query(SQL_CHECK_IS_COURSE_EXIST, ResultSet::next, course.getCourseID());
+    public boolean isValidItemID(Integer courseID) {
+        return jdbcTemplate.query(SQL_CHECK_IS_COURSE_ID_EXIST, ResultSet::next, courseID);
     }
 
     @Override
@@ -60,20 +63,8 @@ public class CourseRepo implements DAO<Course> {
         return jdbcTemplate.queryForObject(SQL_CHECK_IS_COURSE_TABLE_EMPTY, Integer.class) == 0;
     }
 
-
     @Override
-    public boolean removeItem(Course course) {
-        return false;
-    }
-
-    @Override
-    public Optional<Course> getByItemID(Course course) {
-        return Optional.ofNullable(jdbcTemplate.queryForObject(SQL_GET_COURSE_BY_ID, new CourseMapper(),
-                course.getCourseID()));
-    }
-
-    @Override
-    public List<Course> listOfItems(Course course) {
-        return null;
+    public List<Course> listOfItems() {
+        return jdbcTemplate.query(SQL_GET_LIST_OF_COURSE, new CourseMapper());
     }
 }
--- src/main/java/ua/foxminded/javaspring/consoleMenu/repository/GroupRepo.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/repository/GroupRepo.java
@@ -4,9 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import ua.foxminded.javaspring.consoleMenu.dao.DAO;
 import ua.foxminded.javaspring.consoleMenu.dao.GroupDAO;
-import ua.foxminded.javaspring.consoleMenu.data.tables.sqlScripts.SQLQueryOfCreateTable;
+import ua.foxminded.javaspring.consoleMenu.databaseInitializer.tables.sqlScripts.SQLQueryOfCreateTable;
 import ua.foxminded.javaspring.consoleMenu.model.CounterStudentsAtGroup;
 import ua.foxminded.javaspring.consoleMenu.model.Group;
 import ua.foxminded.javaspring.consoleMenu.rowmapper.CountStudentAtGroupMapper;
@@ -17,17 +16,21 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class GroupRepo implements DAO<Group>, GroupDAO {
+public class GroupRepo implements GroupDAO {
 
     private SQLQueryOfCreateTable queryOfCreateTable;
     private JdbcTemplate jdbcTemplate;
 
     private static final String SQL_CHECK_IS_GROUP_TABLE_EMPTY = "SELECT COUNT(*) FROM groups";
     private static final String SQL_ADD_NEW_GROUP = "insert into groups (group_name) values (?)";
-    private static final String SQL_COUNT_STUDENTS_BY_GROUPS = "select group_name, count(s.student_id) as student_count"
-            + "from students s" + "join groups g on s.group_id = g.group_id" + "group by g.group_name"
-            + "having count(s.student_id)<=21";
-    private static final String SQL_GET_GROUP_BY_ID = "select * from course where course_id=?";
+    private static final String SQL_COUNT_STUDENTS_BY_GROUPS = "SELECT g.group_name, COUNT(s.student_id) AS student_count\n"
+            + "FROM groups g\n"
+            + "LEFT JOIN students s ON g.group_id = s.group_id\n"
+            + "GROUP BY g.group_id\n"
+            + "HAVING COUNT(s.student_id) <=?";
+    private static final String SQL_GET_GROUP_BY_ID = "select * from groups where course_id=?";
+    private static final String SQL_CHECK_IS_GROUP_ID_EXIST = "select group_id from groups where group_id=?";
+    private static final String SQL_GET_LIST_OF_GROUPS = "select * from groups";
 
     @Value("${sqlQuery.IsTableExist.SQL_CHECK_IS_TABLE_EXIST}")
     private String SQL_CHECK_IS_TABLE_EXIST;
@@ -41,13 +44,13 @@ public class GroupRepo implements DAO<Group>, GroupDAO {
     }
 
     @Override
-    public List<CounterStudentsAtGroup> counterStudentsAtGroups(int count) {
+    public List<CounterStudentsAtGroup> counterStudentsAtGroups(Integer count) {
         return jdbcTemplate.query(SQL_COUNT_STUDENTS_BY_GROUPS, new CountStudentAtGroupMapper(), count);
     }
 
     @Override
-    public boolean isValidItemID(Group group) {
-        return jdbcTemplate.query(SQL_COUNT_STUDENTS_BY_GROUPS, ResultSet::next, group.getGroupID());
+    public boolean isValidItemID(Integer groupID) {
+        return jdbcTemplate.query(SQL_CHECK_IS_GROUP_ID_EXIST, ResultSet::next, groupID);
     }
 
     @Override
@@ -71,18 +74,7 @@ public class GroupRepo implements DAO<Group>, GroupDAO {
     }
 
     @Override
-    public boolean removeItem(Group item) {
-        return false;
-    }
-
-    @Override
-    public Optional<Group> getByItemID(Group group) {
-        return Optional.ofNullable(jdbcTemplate.queryForObject(SQL_GET_GROUP_BY_ID, new GroupMapper(),
-                group.getGroupID()));
-    }
-
-    @Override
-    public List<Group> listOfItems(Group item) {
-        return null;
+    public List<Group> listOfItems() {
+        return jdbcTemplate.query(SQL_GET_LIST_OF_GROUPS, new GroupMapper());
     }
 }
