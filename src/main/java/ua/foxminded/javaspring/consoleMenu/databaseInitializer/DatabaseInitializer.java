c5e0a6e185adf2b2ea4a3734ac82b8bbbe7e31b8
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-11-14 11:15:34 +0100

    Refactor -Remove unneccessary dependencies
    -Add MenuInteraction dependency to run options menu

--- src/main/java/ua/foxminded/javaspring/consoleMenu/databaseInitializer/DatabaseInitializer.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/databaseInitializer/DatabaseInitializer.java
@@ -1,41 +1,31 @@
-package ua.foxminded.javaspring.consoleMenu.data;
+package ua.foxminded.javaspring.consoleMenu.databaseInitializer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import ua.foxminded.javaspring.consoleMenu.data.tables.TableInitializer;
+import ua.foxminded.javaspring.consoleMenu.databaseInitializer.tables.TableInitializer;
 import ua.foxminded.javaspring.consoleMenu.model.Course;
 import ua.foxminded.javaspring.consoleMenu.model.Group;
 import ua.foxminded.javaspring.consoleMenu.model.Student;
 import ua.foxminded.javaspring.consoleMenu.model.StudentAtCourse;
-import ua.foxminded.javaspring.consoleMenu.options.Menu;
-import ua.foxminded.javaspring.consoleMenu.options.controller.groupOption.OutputStudentsAtGroupByCount;
-import ua.foxminded.javaspring.consoleMenu.options.input.TestCheck;
+import ua.foxminded.javaspring.consoleMenu.options.menu.MenuInteraction;
 
 import javax.annotation.PostConstruct;
 
 @Component
 public class DatabaseInitializer {
     private TableInitializer<Course> courseTableInitializer1;
-
     private TableInitializer<Group> groupTableInitializer;
-
     private TableInitializer<Student> studentTableInitializer;
-
     private TableInitializer<StudentAtCourse> studentAtCourseTableInitializer;
-
-    private Menu menu;
-    private OutputStudentsAtGroupByCount atGroupByCount;
-    private TestCheck testCheck;
+    private MenuInteraction menuInteraction;
 
     @Autowired
-    public DatabaseInitializer(TableInitializer<Course> courseTableInitializer1, TableInitializer<Group> groupTableInitializer, TableInitializer<Student> studentTableInitializer, TableInitializer<StudentAtCourse> studentAtCourseTableInitializer, Menu menu, OutputStudentsAtGroupByCount atGroupByCount, TestCheck testCheck) {
+    public DatabaseInitializer(TableInitializer<Course> courseTableInitializer1, TableInitializer<Group> groupTableInitializer, TableInitializer<Student> studentTableInitializer, TableInitializer<StudentAtCourse> studentAtCourseTableInitializer, MenuInteraction menuInteraction) {
         this.courseTableInitializer1 = courseTableInitializer1;
         this.groupTableInitializer = groupTableInitializer;
         this.studentTableInitializer = studentTableInitializer;
         this.studentAtCourseTableInitializer = studentAtCourseTableInitializer;
-        this.menu = menu;
-        this.atGroupByCount = atGroupByCount;
-        this.testCheck = testCheck;
+        this.menuInteraction = menuInteraction;
     }
 
     @PostConstruct
@@ -48,10 +38,6 @@ public class DatabaseInitializer {
 
         studentAtCourseTableInitializer.initialize();
 
-        System.out.println(menu.getOptions());
-
-//        atGroupByCount.groupsByCount();
-
-        testCheck.testCheck();
+        menuInteraction.startMenu();
     }
 }
