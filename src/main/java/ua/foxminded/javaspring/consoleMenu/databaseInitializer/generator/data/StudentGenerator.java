8b5d7e73832c8d130c697837ed189502fd0c5948
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-11-20 14:26:14 +0100

    Refactor -Add class variable to store count of group -Change using Map instead HashSet -Move initialize maxCountOfStudents to constructor -Unite studentNameRandomCombiner method to generate method -Remove addRandomGroup instead it generateRandomGroupId that  just return random ingex of group -Create method to get count of group -Create method to return student ID

--- src/main/java/ua/foxminded/javaspring/consoleMenu/databaseInitializer/generator/data/StudentGenerator.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/databaseInitializer/generator/data/StudentGenerator.java
@@ -8,73 +8,67 @@ import ua.foxminded.javaspring.consoleMenu.databaseInitializer.generator.sourceD
 import ua.foxminded.javaspring.consoleMenu.model.Student;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class StudentGenerator implements DataGenerator<Student> {
 
     private RandomNumber randomNumber;
     private ResourcesFilesDatabaseData resourcesFiles;
-    private CountConfig countConfig;
     private Integer maxCountOfStudents;
     private DataConduct dataConduct;
-    private HashSet<Student> studentsNames = new HashSet<>();
+    private Integer countOfGroups;
+
+    private Map<String, Student> studentMap = new HashMap<>();
 
     @Autowired
     public StudentGenerator(RandomNumber randomNumber, ResourcesFilesDatabaseData resourcesFiles, CountConfig countConfig, DataConduct dataConduct) {
         this.dataConduct = dataConduct;
         this.randomNumber = randomNumber;
         this.resourcesFiles = resourcesFiles;
-        this.countConfig = countConfig;
+        this.maxCountOfStudents = countConfig.getMaxCountOfStudents();
     }
 
     @Override
     public List<Student> generate() {
-        maxCountOfStudents = countConfig.getMaxCountOfStudents();
+        getGroupCount();
 
-        studentNameRandomCombiner();
-
-        return addRandomGroup();
-    }
-
-    private void studentNameRandomCombiner() {
         List<String> firstNames = resourcesFiles.getFirstNames();
         List<String> lastNames = resourcesFiles.getLastNames();
 
         int countFirstNames = firstNames.size();
         int countLastNames = lastNames.size();
 
-        while (studentsNames.size() < maxCountOfStudents) {
-            int randomFirstNameIndex = randomNumber.generateBetweenOneAndInputNumber(countFirstNames);
-            int randomLastNameIndex = randomNumber.generateBetweenOneAndInputNumber(countLastNames);
+        while (studentMap.size() < maxCountOfStudents) {
+            String firstName = firstNames.get(randomNumber.generateInRange(countFirstNames) - 1);
+            String lastName = lastNames.get(randomNumber.generateInRange(countLastNames) - 1);
 
-            String firstName = firstNames.get(randomFirstNameIndex - 1);
-            String lastName = lastNames.get(randomLastNameIndex - 1);
-
-            Student student = new Student(firstName, lastName);
-            if (!firstName.equals(lastName) && !studentsNames.contains(student)) {
-                studentsNames.add(student);
+            String key = firstName + lastName;
+            if (!firstName.equals(lastName) && !studentMap.containsKey(key)) {
+                studentMap.put(key, new Student(
+                        getNextStudentId(),
+                        firstName,
+                        lastName,
+                        generateRandomGroupId()));
             }
         }
+        List<Student> generatedStudents = new ArrayList<>(studentMap.values());
+
+        dataConduct.setStudents(generatedStudents);
+
+        return generatedStudents;
     }
 
-    private List<Student> addRandomGroup() {
-        int countOfGroups = dataConduct.getGroups().size();
-        int randomGroupIndex;
+    private Long generateRandomGroupId() {
+        return (long) randomNumber.generateInRange(countOfGroups);
+    }
 
-        Long studentID = 1L;
+    private void getGroupCount(){
+        countOfGroups = dataConduct.getGroups().size();
+    }
 
-        List<Student> generatedStudents = new ArrayList<>();
-
-        for (Student student : studentsNames) {
-
-            randomGroupIndex = randomNumber.generateBetweenOneAndInputNumber(countOfGroups);
-
-            generatedStudents.add(new Student(studentID, student.getFirstName(), student.getLastName(),
-                    Long.valueOf(randomGroupIndex)));
-            studentID++;
-        }
-        dataConduct.setStudents(generatedStudents);
-        return generatedStudents;
+    private Long getNextStudentId() {
+        return (long) studentMap.size() + 1;
     }
 }
