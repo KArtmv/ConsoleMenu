6f2b41b6e93dfa6ea2fce830fab84b7b64279254
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-11-27 11:44:02 +0100

    Add dependency StudentService and ConsoleInput classes -Add try-catch block into addStudentToCourse method  where it catch and logging exceptions -Add declaration of throw InvalidIdException to necessary methods -Add verification is given ID is refer to necessary student -Refactor add method, use try-cath instead if-else

--- src/main/java/ua/foxminded/javaspring/consoleMenu/options/controller/studentAtCourseOption/AddStudentToCourse.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/options/controller/studentAtCourseOption/AddStudentToCourse.java
@@ -3,12 +3,17 @@ package ua.foxminded.javaspring.consoleMenu.options.controller.studentAtCourseOp
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import ua.foxminded.javaspring.consoleMenu.exception.InvalidIdException;
 import ua.foxminded.javaspring.consoleMenu.model.Course;
 import ua.foxminded.javaspring.consoleMenu.model.Student;
 import ua.foxminded.javaspring.consoleMenu.model.StudentAtCourse;
+import ua.foxminded.javaspring.consoleMenu.options.console.input.ConsoleInput;
 import ua.foxminded.javaspring.consoleMenu.options.console.input.InputID;
 import ua.foxminded.javaspring.consoleMenu.options.console.output.OutputListOfCourses;
 import ua.foxminded.javaspring.consoleMenu.service.StudentAtCourseService;
+import ua.foxminded.javaspring.consoleMenu.service.StudentService;
+
+import java.util.InputMismatchException;
 
 public class AddStudentToCourse {
 
@@ -18,36 +23,56 @@ public class AddStudentToCourse {
     private StudentAtCourseService studentAtCourseService;
     private InputID<Course> courseInputID;
     private InputID<Student> studentInputID;
+    private StudentService studentService;
+    private ConsoleInput consoleInput;
 
     @Autowired
     public AddStudentToCourse(OutputListOfCourses outputListOfCourses, StudentAtCourseService studentAtCourseService, InputID<Course> courseInputID,
-                              InputID<Student> studentInputID) {
+                              InputID<Student> studentInputID, StudentService studentService, ConsoleInput consoleInput) {
         this.outputListOfCourses = outputListOfCourses;
         this.studentAtCourseService = studentAtCourseService;
         this.courseInputID = courseInputID;
         this.studentInputID = studentInputID;
+        this.studentService = studentService;
+        this.consoleInput = consoleInput;
     }
 
     public void addStudentToCourse() {
-        add(new StudentAtCourse(getStudent(), getCourse()));
+        try {
+            Student student = getStudent();
+            if (verifyValidStudent(student)) {
+                add(new StudentAtCourse(student, getCourse()));
+            }
+        } catch (InvalidIdException | InputMismatchException e) {
+            LOGGER.info("Error adding student to course: " + e.getMessage());
+        }
     }
 
-    private Student getStudent() {
+    private Student getStudent() throws InvalidIdException {
         System.out.println("Input student ID.");
         return new Student(studentInputID.inputID());
     }
 
-    private Course getCourse() {
+    private boolean verifyValidStudent(Student student) {
+        Student selectedStudent = studentService.getStudentByID(student);
+        System.out.printf("Received ID of student which should to add to course: %s %s.\n",
+                selectedStudent.getFirstName(), selectedStudent.getLastName());
+        System.out.print("Confirm that the student is correct.\nInsert 'yes' to confirm or any other key to cancel.");
+
+        return consoleInput.inputCharacters().equalsIgnoreCase("yes");
+    }
+
+    private Course getCourse() throws InvalidIdException {
         outputListOfCourses.viewAllCourses();
         System.out.println("Input course ID, choose from list.");
         return new Course(courseInputID.inputID());
     }
 
     private void add(StudentAtCourse studentAtCourse) {
-        if (studentAtCourseService.addStudentToCourse(studentAtCourse)) {
-            System.out.println("Success, the student had been added to course.");
-        } else {
-            LOGGER.info("Failed to add the student to course.");
+        try {
+            studentAtCourseService.addStudentToCourse(studentAtCourse);
+        } catch (Exception e) {
+            LOGGER.error("Failed add student to course: " + e.getMessage());
         }
     }
 }
