f79f6f56cad04e64fc37e0a2016b5bcc8ab56a4b
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-11-27 11:20:49 +0100

    Add Logger to class -Add try block to groupsByCount method  as well add catch block which catch InputMismatchException  and logging that -Move sort studentsAtGroups list after cheking isEmpty -Provid more explicit instructions for the user

--- src/main/java/ua/foxminded/javaspring/consoleMenu/options/controller/groupOption/OutputStudentsAtGroupByCount.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/options/controller/groupOption/OutputStudentsAtGroupByCount.java
@@ -1,14 +1,19 @@
 package ua.foxminded.javaspring.consoleMenu.options.controller.groupOption;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
 import ua.foxminded.javaspring.consoleMenu.model.CounterStudentsAtGroup;
 import ua.foxminded.javaspring.consoleMenu.options.console.input.ConsoleInput;
 import ua.foxminded.javaspring.consoleMenu.service.GroupService;
 
+import java.util.InputMismatchException;
 import java.util.List;
 
 public class OutputStudentsAtGroupByCount {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OutputStudentsAtGroupByCount.class);
 
     private GroupService groupService;
     private ConsoleInput consoleInput;
@@ -20,15 +25,19 @@ public class OutputStudentsAtGroupByCount {
     }
 
     public void groupsByCount() {
-        System.out.println("Input count of students at course.");
-        int receivedCountOfStudent = consoleInput.inputNumbers();
-        List<CounterStudentsAtGroup> studentsAtGroups = groupService.counterStudentsAtGroups(receivedCountOfStudent);
-        studentsAtGroups.sort((a, b) -> Long.compare(b.getStudentsCount(), a.getStudentsCount()));
+        try {
+            System.out.println("Input the number of students enrolled in a group to find groups with that number or fewer students.");
+            int receivedAmountOfStudent = consoleInput.inputNumbers();
+            List<CounterStudentsAtGroup> studentsAtGroups = groupService.counterStudentsAtGroups(receivedAmountOfStudent);
 
-        if (CollectionUtils.isEmpty(studentsAtGroups)) {
-            System.out.printf("Could not find a group with %d or fewer students.\n", receivedCountOfStudent);
-        } else {
-            outputResult(studentsAtGroups);
+            if (CollectionUtils.isEmpty(studentsAtGroups)) {
+                System.out.printf("Could not find a group with %d or fewer students.\n", receivedAmountOfStudent);
+            } else {
+                studentsAtGroups.sort((a, b) -> Long.compare(b.getStudentsCount(), a.getStudentsCount()));
+                outputResult(studentsAtGroups);
+            }
+        } catch (InputMismatchException e) {
+            LOGGER.info("Error getting numbers:" + e.getMessage());
         }
     }
 
