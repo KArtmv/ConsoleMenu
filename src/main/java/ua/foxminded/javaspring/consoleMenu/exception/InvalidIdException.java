b873fbcde82417c48075f661d17ad999f705e951
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-11-27 12:40:33 +0100

    Create exception

---
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/exception/InvalidIdException.java
@@ -0,0 +1,8 @@
+package ua.foxminded.javaspring.consoleMenu.exception;
+
+public class InvalidIdException extends Exception {
+
+    public InvalidIdException(String message) {
+        super(message);
+    }
+}
