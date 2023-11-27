7b4ba36dd5f5c416a68cbadef6f2ed574ed951ad
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-11-27 10:40:37 +0100

    Refactor -Remove logging -Add declare Exception at methods -Change InputMismatchException instead IllegalArgumentException  in inputNumbers method -Refactor input method  remove do-while loop, instead try to take  correct data if input not match data by do-while  now it thorow exception -Remove unnecessary handleInvalidInput method -Instead logging use throw exception

--- src/main/java/ua/foxminded/javaspring/consoleMenu/options/console/input/ConsoleInput.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/options/console/input/ConsoleInput.java
@@ -1,8 +1,6 @@
 package ua.foxminded.javaspring.consoleMenu.options.console.input;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ConsoleInput {
@@ -12,45 +10,32 @@ public class ConsoleInput {
     private static final String INTEGER_PATTERN = "\\d+";
     private static final String MENU_PATTERN = "[1-9x]";
 
-    private static final Logger logger = LoggerFactory.getLogger(ConsoleInput.class);
-
-
     public ConsoleInput() {
         this.sc = new Scanner(System.in);
     }
 
-    public Integer inputNumbers() {
-        int receivedNumber;
+    public Integer inputNumbers() throws InputMismatchException {
         try {
-            receivedNumber = Integer.parseInt(input(INTEGER_PATTERN));
+            return Integer.parseInt(input(INTEGER_PATTERN));
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Failed to converted input number.");
+            throw new InputMismatchException("Failed to converted input number." + e.getMessage());
         }
-        return receivedNumber;
     }
 
-    public String inputCharacters() {
+    public String inputCharacters() throws InputMismatchException {
         return input(ALPHABETIC_PATTERN);
     }
 
-    public String menuInput() {
+    public String menuInput() throws InputMismatchException {
         return input(MENU_PATTERN);
     }
 
-    private String input(String regex) {
-        String input;
-        do {
-            input = sc.nextLine();
-        } while (handleInvalidInput(input, regex));
-        return input;
-    }
-
-    private boolean handleInvalidInput(String input, String regex) {
-        boolean isValid = false;
-        if (input == null || input.isEmpty() || input.matches("\\s+") || !input.matches(regex)) {
-            isValid = true;
-            logger.info("Received an illegal argument. Try again.");
+    private String input(String regex){
+        String s = sc.nextLine();
+        if (s.matches(regex)){
+            return s;
+        } else {
+            throw new InputMismatchException(String.format("Input: '%s', does not match the required pattern: '%s'.", s, regex));
         }
-        return isValid;
     }
 }
