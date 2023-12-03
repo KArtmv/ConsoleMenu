ed6d6001b88fd1a29f090d120a5eb77989c41a44
”Kyrylo <kirillartemov1993@gmail.com>
2023-12-03 11:11:26 +0100

    Refactor GroupService and its implement -Change counterStudentAtGroup method to void -Move logic from OutputStudentAtGroupByCount to GroupServiceImpl

--- src/main/java/ua/foxminded/javaspring/consoleMenu/service/impl/GroupServiceImpl.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/service/impl/GroupServiceImpl.java
@@ -1,26 +1,51 @@
 package ua.foxminded.javaspring.consoleMenu.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import ua.foxminded.javaspring.consoleMenu.dao.GroupDAO;
 //import ua.foxminded.javaspring.consoleMenu.model.CounterStudentsAtGroup;
 import ua.foxminded.javaspring.consoleMenu.model.CounterStudentsAtGroup;
+import ua.foxminded.javaspring.consoleMenu.options.console.input.ConsoleInput;
+import ua.foxminded.javaspring.consoleMenu.options.console.output.ConsolePrinter;
 import ua.foxminded.javaspring.consoleMenu.service.GroupService;
 
+import java.util.InputMismatchException;
 import java.util.List;
 
 @Service
 public class GroupServiceImpl implements GroupService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GroupServiceImpl.class);
+
     private GroupDAO groupDAO;
+    private ConsoleInput consoleInput;
+    private ConsolePrinter consolePrinter;
 
     @Autowired
-    public GroupServiceImpl(GroupDAO groupDAO) {
+    public GroupServiceImpl(GroupDAO groupDAO, ConsoleInput consoleInput, ConsolePrinter consolePrinter) {
         this.groupDAO = groupDAO;
+        this.consoleInput = consoleInput;
+        this.consolePrinter = consolePrinter;
     }
 
     @Override
-    public List<CounterStudentsAtGroup> counterStudentsAtGroups(Integer count) {
-        return groupDAO.counterStudentsAtGroups(count);
+    public void counterStudentsAtGroups(Integer count) {
+        try {
+            System.out.println("Input the number of students enrolled in a group to find groups with that number or fewer students.");
+            int receivedAmountOfStudent = consoleInput.inputNumbers();
+            List<CounterStudentsAtGroup> studentsAtGroups = groupDAO.counterStudentsAtGroups(receivedAmountOfStudent);
+
+            if (CollectionUtils.isEmpty(studentsAtGroups)) {
+                System.out.printf("Could not find a group with %d or fewer students.\n", receivedAmountOfStudent);
+            } else {
+                studentsAtGroups.sort((a, b) -> Long.compare(b.getStudentsCount(), a.getStudentsCount()));
+                consolePrinter.viewAmountStudentAtGroup(studentsAtGroups);
+            }
+        } catch (InputMismatchException e) {
+            LOGGER.info("Error getting numbers:" + e.getMessage());
+        }
     }
 }
