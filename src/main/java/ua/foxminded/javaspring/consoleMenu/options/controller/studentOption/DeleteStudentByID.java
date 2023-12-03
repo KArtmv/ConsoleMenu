a63ff69a4d16051328911fd492a0e44b8ea5bdbd
”Kyrylo <kirillartemov1993@gmail.com>
2023-12-03 11:16:40 +0100

    Refactor StudentService and its implement -Move logic from AddNewStudent, AddStudentToCourse, DeleteStudent,  RemoveStudentFromSpecifyCourse to StudentService to relate methods -Change contains methods to void

--- src/main/java/ua/foxminded/javaspring/consoleMenu/options/controller/studentOption/DeleteStudentByID.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/options/controller/studentOption/DeleteStudentByID.java
@@ -1,55 +1,27 @@
-package ua.foxminded.javaspring.consoleMenu.options.controller.studentOption;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import ua.foxminded.javaspring.consoleMenu.exception.InvalidIdException;
-import ua.foxminded.javaspring.consoleMenu.model.Student;
-import ua.foxminded.javaspring.consoleMenu.options.StudentConfirmationHandler;
-import ua.foxminded.javaspring.consoleMenu.options.console.input.ItemID;
-//import ua.foxminded.javaspring.consoleMenu.service.StudentAtCourseService;
-import ua.foxminded.javaspring.consoleMenu.service.StudentService;
-
-public class DeleteStudentByID {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(DeleteStudentByID.class);
-
-    private ItemID<Student> inputID;
-    private StudentService studentService;
-    private StudentConfirmationHandler verifyValidStudent;
-
-    @Autowired
-    public DeleteStudentByID(ItemID<Student> inputID, StudentService studentService, StudentConfirmationHandler verifyValidStudent) {
-        this.inputID = inputID;
-        this.studentService = studentService;
-        this.verifyValidStudent = verifyValidStudent;
-    }
-
-    public void remove() {
-        try {
-            Student student = getStudentID();
-            if (verifyValidStudent.verifyValidStudent(student)) {
-                removing(student);
-            }
-        } catch (InvalidIdException e) {
-            LOGGER.info("Error getting student ID: {}", e.getMessage());
-        }
-    }
-
-    private Student getStudentID() throws InvalidIdException {
-        System.out.println("Enter the ID of the student you want to remove.");
-        return new Student(inputID.inputID());
-    }
-
-
-    private void removing(Student student) {
-        try {
-            if (!studentService.allCoursesOfStudent(student).isEmpty()) {
-                studentService.removeStudentFromAllTheirCourses(student);
-            }
-            studentService.deleteStudent(student);
-        } catch (Exception e) {
-            LOGGER.error("Failed removing student: {}", e.getMessage());
-        }
-    }
-}
+//package ua.foxminded.javaspring.consoleMenu.options.controller.studentOption;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import ua.foxminded.javaspring.consoleMenu.exception.InvalidIdException;
+//import ua.foxminded.javaspring.consoleMenu.model.Student;
+//import ua.foxminded.javaspring.consoleMenu.options.StudentConfirmationHandler;
+//import ua.foxminded.javaspring.consoleMenu.options.console.input.ItemID;
+////import ua.foxminded.javaspring.consoleMenu.service.StudentAtCourseService;
+//import ua.foxminded.javaspring.consoleMenu.service.StudentService;
+//
+//public class DeleteStudentByID {
+//
+//    private static final Logger LOGGER = LoggerFactory.getLogger(DeleteStudentByID.class);
+//
+//    private StudentService studentService;
+//    private ItemID<Student> inputID;
+//    private StudentConfirmationHandler verifyValidStudent;
+//
+//    @Autowired
+//    public DeleteStudentByID(ItemID<Student> inputID, StudentService studentService, StudentConfirmationHandler verifyValidStudent) {
+//        this.inputID = inputID;
+//        this.studentService = studentService;
+//        this.verifyValidStudent = verifyValidStudent;
+//    }
+//}
--- src/main/java/ua/foxminded/javaspring/consoleMenu/service/impl/StudentServiceImpl.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/service/impl/StudentServiceImpl.java
@@ -1,66 +1,117 @@
 package ua.foxminded.javaspring.consoleMenu.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import ua.foxminded.javaspring.consoleMenu.dao.StudentAtCourseDAO;
 import ua.foxminded.javaspring.consoleMenu.dao.StudentDAO;
+import ua.foxminded.javaspring.consoleMenu.exception.InvalidIdException;
+import ua.foxminded.javaspring.consoleMenu.model.Course;
 import ua.foxminded.javaspring.consoleMenu.model.Student;
 import ua.foxminded.javaspring.consoleMenu.model.StudentAtCourse;
+import ua.foxminded.javaspring.consoleMenu.options.StudentConfirmationHandler;
+import ua.foxminded.javaspring.consoleMenu.options.console.input.ItemID;
+import ua.foxminded.javaspring.consoleMenu.options.console.input.NewStudentData;
+import ua.foxminded.javaspring.consoleMenu.options.console.output.ConsolePrinter;
+import ua.foxminded.javaspring.consoleMenu.options.print.ItemPrint;
 import ua.foxminded.javaspring.consoleMenu.service.StudentService;
 
+import java.util.InputMismatchException;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class StudentServiceImpl implements StudentService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(StudentServiceImpl.class);
+
     private StudentDAO studentDAO;
     private StudentAtCourseDAO studentAtCourseDAO;
+    private ItemID<Student> studentItemID;
+    private StudentConfirmationHandler verifyValidStudent;
+    private NewStudentData newStudentData;
+    private ConsolePrinter consolePrinter;
+    private ItemID<StudentAtCourse> studentAtCourseInputID;
+    private ItemPrint<Course> printAllCourses;
+    private ItemID<Course> courseItemID;
 
-    @Autowired
-    public StudentServiceImpl(StudentDAO studentDAO, StudentAtCourseDAO studentAtCourseDAO) {
+
+    public StudentServiceImpl(StudentDAO studentDAO, StudentAtCourseDAO studentAtCourseDAO, ItemID<Student> studentItemID,
+                              StudentConfirmationHandler verifyValidStudent, NewStudentData newStudentData,
+                              ConsolePrinter consolePrinter, ItemID<StudentAtCourse> studentAtCourseInputID,
+                              ItemPrint<Course> printAllCourses, ItemID<Course> courseItemID) {
         this.studentDAO = studentDAO;
         this.studentAtCourseDAO = studentAtCourseDAO;
+        this.studentItemID = studentItemID;
+        this.verifyValidStudent = verifyValidStudent;
+        this.newStudentData = newStudentData;
+        this.consolePrinter = consolePrinter;
+        this.studentAtCourseInputID = studentAtCourseInputID;
+        this.printAllCourses = printAllCourses;
+        this.courseItemID = courseItemID;
     }
 
     @Override
-    public void saveStudent(Student student) {
-        studentDAO.addItem(student);
-    }
-
-    @Override
-    public List<StudentAtCourse> allCoursesOfStudent(Student studentID) {
-        return studentDAO.studentCourses(studentID);
+    public void addNewStudent(Student student) {
+        System.out.println("Input data of a student.");
+        try {
+            studentDAO.addItem(newStudentData.get());
+            System.out.println("Success, student had been added");
+        } catch (InputMismatchException | InvalidIdException e) {
+            LOGGER.info("Failed getting student data: {}", e.getMessage());
+        }
     }
 
     @Override
     public void deleteStudent(Student studentID) {
-        studentDAO.removeStudent(studentID);
-    }
-
-    @Override
-    public Student getStudentByID(Student student) {
-        Optional<Student> result = studentDAO.getByItemID(student);
-
-        Student resultStudent = null;
-        if (result.isPresent()) {
-            resultStudent = result.get();
+        try {
+            System.out.println("Enter the ID of the student you want to remove.");
+            Student student = new Student(studentItemID.inputID());
+            if (verifyValidStudent.verifyValidStudent(student)) {
+                studentAtCourseDAO.removeStudentFromAllTheirCourses(student);
+                studentDAO.removeStudent(student);
+            }
+        } catch (InvalidIdException e) {
+            LOGGER.info("Error getting student ID: {}", e.getMessage());
         }
-        return resultStudent;
     }
 
     @Override
-    public boolean addStudentToCourse(StudentAtCourse studentAtCourse) {
-        return studentAtCourseDAO.addItem(studentAtCourse);
+    public void addStudentToCourse(StudentAtCourse studentAtCourse) {
+        try {
+            System.out.println("Input student ID.");
+            Student student = new Student(studentItemID.inputID());
+            if (verifyValidStudent.verifyValidStudent(student)) {
+                System.out.println("Input course ID, choose from list.");
+                printAllCourses.printAllAvailableItems();
+                studentAtCourseDAO.addItem(new StudentAtCourse(student, new Course(courseItemID.inputID())));
+            }
+        } catch (InvalidIdException | InputMismatchException e) {
+            LOGGER.info("Error adding student to course: {}", e.getMessage());
+        }
     }
 
     @Override
     public void removeStudentFromCourse(StudentAtCourse studentAtCourse) {
-        studentAtCourseDAO.removeStudentFromCourse(studentAtCourse);
-    }
+        try {
+            System.out.println("Input the ID of student which should be remove from course. Then press enter.");
+            Student student = new Student(studentItemID.inputID());
+            List<StudentAtCourse> allStudentCourses = studentDAO.studentCourses(student);
 
-    @Override
-    public void removeStudentFromAllTheirCourses(Student student) {
-        studentAtCourseDAO.removeStudentFromAllTheirCourses(student);
+            if (!CollectionUtils.isEmpty(allStudentCourses)) {
+                System.out.println("Choose enrollment ID from the list to wish remove and press enter.");
+                consolePrinter.viewAllCoursesOfStudent(allStudentCourses);
+                Long receivedID = studentAtCourseInputID.inputID();
+
+                if (allStudentCourses.stream().anyMatch(c -> c.getEnrollmentID().equals(receivedID))) {
+                    studentAtCourseDAO.removeStudentFromCourse(new StudentAtCourse(receivedID));
+                }
+            } else {
+                Student studentWithoutCourses = studentDAO.getByItemID(student).get();
+                System.out.printf("The student: %s %s have not relate to any course.\n", studentWithoutCourses.getFirstName(), studentWithoutCourses.getLastName());
+            }
+        } catch (InvalidIdException e) {
+            LOGGER.info("Error getting ID: {}", e.getMessage());
+        }
     }
 }
