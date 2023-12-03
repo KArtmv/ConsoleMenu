672866ffb84f15dfa7382b00bbf02b89ddd9adb7
”Kyrylo <kirillartemov1993@gmail.com>
2023-12-03 11:03:18 +0100

    Refactor CourseSecrvice and its implement -Change showAllStudentFromCourse to void method -Move logic from AllStudentFromCourse class to CourseServiceImpl

--- src/main/java/ua/foxminded/javaspring/consoleMenu/service/CourseService.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/service/CourseService.java
@@ -1,11 +1,8 @@
 package ua.foxminded.javaspring.consoleMenu.service;
 
 import ua.foxminded.javaspring.consoleMenu.model.Course;
-import ua.foxminded.javaspring.consoleMenu.model.StudentAtCourse;
-
-import java.util.List;
 
 public interface CourseService {
-    List<StudentAtCourse> allStudentsFromCourse(Course course);
+    void allStudentsFromCourse(Course course);
 
 }
