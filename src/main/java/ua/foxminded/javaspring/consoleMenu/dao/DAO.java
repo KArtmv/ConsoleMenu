86a3213ebbab9114a69e4b86d00429e5e6bc98ff
”Kyrylo <kirillartemov1993@gmail.com>
2023-12-08 10:32:23 +0100

    Remove isValidItemID, getAll methods -Add getItemByID method

--- src/main/java/ua/foxminded/javaspring/consoleMenu/dao/DAO.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/dao/DAO.java
@@ -1,17 +1,15 @@
 package ua.foxminded.javaspring.consoleMenu.dao;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface DAO<T> {
     boolean addItem(T item);
 
-    boolean isValidItemID(Integer itemID);
+    Optional<T> getItemByID(T t);
 
     boolean isTableExist();
 
     void createTable();
 
     boolean isTableEmpty();
-
-    List<T> getAll();
 }
