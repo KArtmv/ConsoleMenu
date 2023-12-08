624b3c341259623058092f4efcd21210e4549237
”Kyrylo <kirillartemov1993@gmail.com>
2023-12-08 11:33:18 +0100

    Refactor CourseServiceImpl -Move bissnes logic to CourseController class -Change catch exception to catch EmptyResultDataAccessException and then  throw InvalidIdException -Add getAllCourses method

--- src/main/java/ua/foxminded/javaspring/consoleMenu/service/impl/CourseServiceImpl.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/service/impl/CourseServiceImpl.java
@@ -1,16 +1,13 @@
 package ua.foxminded.javaspring.consoleMenu.service.impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
+import ua.foxminded.javaspring.consoleMenu.dao.CourseDAO;
 import ua.foxminded.javaspring.consoleMenu.dao.StudentAtCourseDAO;
 import ua.foxminded.javaspring.consoleMenu.exception.InvalidIdException;
 import ua.foxminded.javaspring.consoleMenu.model.Course;
 import ua.foxminded.javaspring.consoleMenu.model.StudentAtCourse;
-import ua.foxminded.javaspring.consoleMenu.options.console.input.ItemID;
-import ua.foxminded.javaspring.consoleMenu.options.console.output.ConsolePrinter;
 import ua.foxminded.javaspring.consoleMenu.service.CourseService;
 
 import java.util.List;
@@ -18,32 +15,30 @@ import java.util.List;
 @Service
 public class CourseServiceImpl implements CourseService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CourseServiceImpl.class);
     private StudentAtCourseDAO studentAtCourseDAO;
-    private ConsolePrinter consolePrinter;
-    private ItemID<Course> inputID;
+    private CourseDAO courseDAO;
 
     @Autowired
-    public CourseServiceImpl(StudentAtCourseDAO studentAtCourseDAO, ConsolePrinter consolePrinter, ItemID<Course> inputID) {
+    public CourseServiceImpl(StudentAtCourseDAO studentAtCourseDAO, CourseDAO courseDAO) {
         this.studentAtCourseDAO = studentAtCourseDAO;
-        this.consolePrinter = consolePrinter;
-        this.inputID = inputID;
+        this.courseDAO = courseDAO;
     }
 
     @Override
-    public void allStudentsFromCourse() {
+    public List<StudentAtCourse> allStudentsFromCourse(Course course) {
+        List<StudentAtCourse> result = null;
         try {
-            consolePrinter.printAllCourses();
-            System.out.println("Choose and input the ID of the course from the list to view all students enrolled in this course.");
-            List<StudentAtCourse> studentsFromCourse = studentAtCourseDAO.allStudentsFromCourse((new Course(inputID.inputID())));
-
-            if (!CollectionUtils.isEmpty(studentsFromCourse)) {
-                consolePrinter.viewAllStudentsFromCourse(studentsFromCourse);
-            } else {
-                System.out.println("No found students for the selected course.");
+            if (courseDAO.getItemByID(course).isPresent()) {
+                result = studentAtCourseDAO.allStudentsFromCourse(course);
             }
-        } catch (InvalidIdException e) {
-            LOGGER.info("Error getting course ID: " + e.getMessage());
+        } catch (EmptyResultDataAccessException e) {
+            throw new InvalidIdException("No founded course by given ID: " + course.getCourseID());
         }
+        return result;
+    }
+
+    @Override
+    public List<Course> getAllCourses() {
+        return courseDAO.getAll();
     }
 }
