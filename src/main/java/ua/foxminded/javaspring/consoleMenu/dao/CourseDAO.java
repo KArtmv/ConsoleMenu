c38b8e3d74f1fd6715eb045c24d80bf7577f8a4d
”Kyrylo <kirillartemov1993@gmail.com>
2023-12-08 10:29:56 +0100

    Roll back CourseDAO

--- src/main/java/ua/foxminded/javaspring/consoleMenu/dao/CourseDAO.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/dao/CourseDAO.java
@@ -1,9 +1,9 @@
-//package ua.foxminded.javaspring.consoleMenu.dao;
-//
-//import ua.foxminded.javaspring.consoleMenu.model.Course;
-//
-//import java.util.List;
-//
-//public interface CourseDAO extends DAO<Course> {
-//    List<Course> getAll();
-//}
+package ua.foxminded.javaspring.consoleMenu.dao;
+
+import ua.foxminded.javaspring.consoleMenu.model.Course;
+
+import java.util.List;
+
+public interface CourseDAO extends DAO<Course> {
+    List<Course> getAll();
+}
