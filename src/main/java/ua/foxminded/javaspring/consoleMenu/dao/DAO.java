d86776f88903122d273519d28ef0a176c61f283e
”Kyrylo <kirillartemov1993@gmail.com>
2023-12-28 16:27:10 +0100

    Refactor DAO class -Rename DAO to TablesDAO -Separate CRUD methods to DAO class -TablesDAO now responsibilities only tables

--- src/main/java/ua/foxminded/javaspring/consoleMenu/dao/DAO.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/dao/DAO.java
@@ -6,10 +6,4 @@ public interface DAO<T> {
     boolean addItem(T item);
 
     Optional<T> getItemByID(T t);
-
-    boolean isTableExist();
-
-    void createTable();
-
-    boolean isTableEmpty();
 }
