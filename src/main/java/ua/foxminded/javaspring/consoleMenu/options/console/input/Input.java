3d0576ec1889152d1aad957bc65c5cfa00399ccf
”Kyrylo <kirillartemov1993@gmail.com>
2024-01-06 12:44:04 +0100

    Add verifyValidStudent metod -Add StudentService dependency -Change Sysytem.out.print to consolePrinter.print()

--- src/main/java/ua/foxminded/javaspring/consoleMenu/options/console/input/Input.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/options/console/input/Input.java
@@ -3,6 +3,7 @@ package ua.foxminded.javaspring.consoleMenu.options.console.input;
 import org.springframework.beans.factory.annotation.Autowired;
 import ua.foxminded.javaspring.consoleMenu.model.Student;
 import ua.foxminded.javaspring.consoleMenu.options.console.output.ConsolePrinter;
+import ua.foxminded.javaspring.consoleMenu.service.StudentService;
 
 import java.util.InputMismatchException;
 
@@ -10,11 +11,13 @@ public class Input {
     private static final String ALPHABETIC_PATTERN = "\\p{Alpha}+";
     private MyScanner scanner;
     private ConsolePrinter consolePrinter;
+    private StudentService studentService;
 
     @Autowired
-    public Input(MyScanner scanner, ConsolePrinter consolePrinter) {
+    public Input(MyScanner scanner, ConsolePrinter consolePrinter, StudentService studentService) {
         this.scanner = scanner;
         this.consolePrinter = consolePrinter;
+        this.studentService = studentService;
     }
 
     public String inputOptionMenu() {
@@ -26,13 +29,13 @@ public class Input {
     }
 
     public Student getStudent(){
-        System.out.println("Input data of a new student.");
         Student student = new Student();
-        System.out.println("Input student first name and press enter.");
+        consolePrinter.print("Input data of a new student.");
+        consolePrinter.print("Input student first name and press enter.");
         student.setFirstName(getString());
-        System.out.println("Input student last name and press enter.");
+        consolePrinter.print("Input student last name and press enter.");
         student.setLastName(getString());
-        System.out.println("Now you should choose a group from list to which should add student.\n Input ID and press enter.");
+        consolePrinter.print("Now you should choose a group from list to which should add student.\n Input ID and press enter.");
         consolePrinter.printAllGroups();
         student.setGroupID(scanner.getLong());
 
@@ -46,5 +49,13 @@ public class Input {
         } else {
             throw new InputMismatchException(String.format("Input: '%s', does not match the alphabetic pattern.", s));
         }
+    }
+
+    public boolean verifyValidStudent(Student student) {
+        Student selectedStudent = studentService.getStudent(student);
+        consolePrinter.print(String.format("Confirm that the student: student: ID %s, %s %s is correct.\nInsert 'yes' to confirm or any other key to cancel: ",
+                selectedStudent.getStudentID(), selectedStudent.getFirstName(), selectedStudent.getLastName()));
+
+        return scanner.getLine().equalsIgnoreCase("yes");
     }
 }
