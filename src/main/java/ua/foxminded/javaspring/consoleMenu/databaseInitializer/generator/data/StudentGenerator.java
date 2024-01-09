0385dfce15c9f815223898d54f4b1d120df01f9b
”Kyrylo <kirillartemov1993@gmail.com>
2024-01-09 17:25:53 +0100

    Refactor -Remove Value anotation -Add MyRandom, AmountLimit -Remove Random constant -Change using MyRandom instead Random class

--- src/main/java/ua/foxminded/javaspring/consoleMenu/databaseInitializer/generator/data/StudentGenerator.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/databaseInitializer/generator/data/StudentGenerator.java
@@ -1,34 +1,37 @@
 package ua.foxminded.javaspring.consoleMenu.databaseInitializer.generator.data;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import ua.foxminded.javaspring.consoleMenu.databaseInitializer.generator.DataConduct;
 import ua.foxminded.javaspring.consoleMenu.databaseInitializer.generator.sourceData.ResourcesFilesDatabaseData;
 import ua.foxminded.javaspring.consoleMenu.model.Student;
+import ua.foxminded.javaspring.consoleMenu.util.AmountLimit;
+import ua.foxminded.javaspring.consoleMenu.util.MyRandom;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class StudentGenerator implements DataGenerator<Student> {
 
-    public static final AtomicLong ATOMIC_LONG = new AtomicLong(1);
-    private static final Random RANDOM = new Random();
-    @Value("${maxCountOfStudent}")
+    private static final AtomicLong ATOMIC_LONG = new AtomicLong(1);
     private int maxCountOfStudents;
+    private AmountLimit amountLimit;
+    private MyRandom random;
     private ResourcesFilesDatabaseData resourcesFiles;
     private DataConduct dataConduct;
     private int countOfGroups;
     private Map<String, Student> studentMap = new HashMap<>();
 
     @Autowired
-    public StudentGenerator(ResourcesFilesDatabaseData resourcesFiles, DataConduct dataConduct) {
+    public StudentGenerator(AmountLimit amountLimit, MyRandom random, ResourcesFilesDatabaseData resourcesFiles, DataConduct dataConduct) {
+        this.amountLimit = amountLimit;
+        this.random = random;
         this.dataConduct = dataConduct;
         this.resourcesFiles = resourcesFiles;
     }
 
     @Override
     public List<Student> generate() {
-        getCounters();
+        initVariables();
 
         List<String> firstNames = resourcesFiles.getFirstNames();
         List<String> lastNames = resourcesFiles.getLastNames();
@@ -37,8 +40,8 @@ public class StudentGenerator implements DataGenerator<Student> {
         int countLastNames = lastNames.size();
 
         while (studentMap.size() < maxCountOfStudents) {
-            String firstName = firstNames.get(RANDOM.nextInt(countFirstNames - 1) + 1);
-            String lastName = lastNames.get(RANDOM.nextInt(countLastNames - 1) + 1);
+            String firstName = firstNames.get(random.getInt(countFirstNames));
+            String lastName = lastNames.get(random.getInt(countLastNames));
 
             String key = firstName + lastName;
             if (!firstName.equals(lastName) && !studentMap.containsKey(key)) {
@@ -46,7 +49,7 @@ public class StudentGenerator implements DataGenerator<Student> {
                                 ATOMIC_LONG.getAndIncrement(),
                                 firstName,
                                 lastName,
-                                generateRandomGroupId()));
+                                random.getLong(countOfGroups)));
             }
         }
         List<Student> generatedStudents = new ArrayList<>(studentMap.values());
@@ -56,11 +59,8 @@ public class StudentGenerator implements DataGenerator<Student> {
         return generatedStudents;
     }
 
-    private void getCounters() {
+    private void initVariables() {
+        maxCountOfStudents = amountLimit.maxCountOfStudents;
         countOfGroups = dataConduct.getGroups().size();
-    }
-
-    private Long generateRandomGroupId() {
-        return RANDOM.longs(1, (countOfGroups + 1)).limit(1).findFirst().getAsLong();
     }
 }
--- src/main/java/ua/foxminded/javaspring/consoleMenu/databaseInitializer/generator/data/StudentToCourseGenerator.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/databaseInitializer/generator/data/StudentToCourseGenerator.java
@@ -1,32 +1,34 @@
 package ua.foxminded.javaspring.consoleMenu.databaseInitializer.generator.data;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import ua.foxminded.javaspring.consoleMenu.databaseInitializer.generator.DataConduct;
 import ua.foxminded.javaspring.consoleMenu.model.Course;
 import ua.foxminded.javaspring.consoleMenu.model.Student;
 import ua.foxminded.javaspring.consoleMenu.model.StudentAtCourse;
+import ua.foxminded.javaspring.consoleMenu.util.AmountLimit;
+import ua.foxminded.javaspring.consoleMenu.util.MyRandom;
 
 import java.util.*;
 
 public class StudentToCourseGenerator implements DataGenerator<StudentAtCourse> {
-    private static final Random RANDOM = new Random();
-    public Set<Long> INDICES_COURSES_OF_STUDENT = new HashSet<>();
-    @Value("${maxCountCoursesOfStudent}")
     private int maxCountCoursesOfStudent;
     private int countCourses;
     private DataConduct dataConduct;
-
+    private AmountLimit amountLimits;
+    private MyRandom random;
+    private Set<Long> INDICES_COURSES_OF_STUDENT = new HashSet<>();
     private List<StudentAtCourse> studentAtCourses = new ArrayList<>();
 
     @Autowired
-    public StudentToCourseGenerator(DataConduct dataConduct) {
+    public StudentToCourseGenerator(DataConduct dataConduct, AmountLimit amountLimits, MyRandom random) {
         this.dataConduct = dataConduct;
+        this.amountLimits = amountLimits;
+        this.random = random;
     }
 
     @Override
     public List<StudentAtCourse> generate() {
-        getCoursesCount();
+        initVariables();
         List<Student> students = dataConduct.getStudents();
         students.forEach(this::addToCourseByIndex);
 
@@ -43,14 +45,15 @@ public class StudentToCourseGenerator implements DataGenerator<StudentAtCourse> 
     }
 
     private void randomlyCoursesIndex() {
-        int amountStudentCourses = RANDOM.nextInt(maxCountCoursesOfStudent) + 1;
+        int amountStudentCourses = random.getInt(maxCountCoursesOfStudent + 1);
 
         while (INDICES_COURSES_OF_STUDENT.size() < amountStudentCourses) {
-            INDICES_COURSES_OF_STUDENT.add(RANDOM.longs(1, (countCourses + 1)).limit(1).findFirst().getAsLong());
+            INDICES_COURSES_OF_STUDENT.add(random.getLong(countCourses));
         }
     }
 
-    private void getCoursesCount() {
+    private void initVariables() {
         countCourses = dataConduct.getCourses().size();
+        maxCountCoursesOfStudent = amountLimits.maxCountCoursesOfStudent;
     }
 }
