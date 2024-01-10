c89b928d369a74244181da2b692e51341841ce5f
”Kyrylo <kirillartemov1993@gmail.com>
2024-01-10 16:25:56 +0100

    Refactor test -Rewritting tests

--- src/test/java/ua/foxminded/javaspring/consoleMenu/data/generator/StudentToCourseGeneratorTest.java
+++ src/test/java/ua/foxminded/javaspring/consoleMenu/data/generator/StudentToCourseGeneratorTest.java
@@ -2,31 +2,32 @@ package ua.foxminded.javaspring.consoleMenu.data.generator;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
-import ua.foxminded.javaspring.consoleMenu.data.RandomNumber;
-import ua.foxminded.javaspring.consoleMenu.data.generator.sourceData.CountConfig;
+import ua.foxminded.javaspring.consoleMenu.databaseInitializer.generator.DataConduct;
+import ua.foxminded.javaspring.consoleMenu.databaseInitializer.generator.data.StudentToCourseGenerator;
 import ua.foxminded.javaspring.consoleMenu.model.Course;
 import ua.foxminded.javaspring.consoleMenu.model.Student;
 import ua.foxminded.javaspring.consoleMenu.model.StudentAtCourse;
 import ua.foxminded.javaspring.consoleMenu.pattern.InitializeObject;
+import ua.foxminded.javaspring.consoleMenu.util.AmountLimit;
+import ua.foxminded.javaspring.consoleMenu.util.MyRandom;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.Mockito.*;
 
-@RunWith(MockitoJUnitRunner.class)
-public class StudentToCourseGeneratorTest {
+class StudentToCourseGeneratorTest {
     @Mock
-    private RandomNumber randomNumber;
-
+    DataConduct dataConduct;
     @Mock
-    private CountConfig countConfig;
-
+    AmountLimit amountLimit;
+    @Mock
+    private MyRandom random;
     @InjectMocks
     private StudentToCourseGenerator studentToCourseGenerator;
 
@@ -38,27 +39,35 @@ public class StudentToCourseGeneratorTest {
     @Test
     void generate_shouldReturnListOfStudentAtCourse_whenIsCorrect() {
         InitializeObject initializeObject = new InitializeObject();
-        List<Student> students = initializeObject.studentsListInit();
-
-        int countCourses = 10;
+        List<Student> students =  initializeObject.studentsListInit();
+        List<Course> courses = initializeObject.coursesListInit();
         int maxCountCoursesOfStudent = 3;
+        
+        List<StudentAtCourse> expect = new ArrayList<>();
+        expect.add(new StudentAtCourse(new Student(1L, "firstname1", "lastName1", 1L), new Course(1L)));
+        expect.add(new StudentAtCourse(new Student(1L, "firstname1", "lastName1", 1L), new Course(2L)));
+        expect.add(new StudentAtCourse(new Student(2L, "firstname2", "lastName2", 2L), new Course(1L)));
+        expect.add(new StudentAtCourse(new Student(3L, "firstname3", "lastName3", 3L), new Course(1L)));
+        expect.add(new StudentAtCourse(new Student(3L, "firstname3", "lastName3", 3L), new Course(2L)));
+        expect.add(new StudentAtCourse(new Student(3L, "firstname3", "lastName3", 3L), new Course(3L)));
 
-        when(countConfig.getMaxCountCoursesOfStudent()).thenReturn(maxCountCoursesOfStudent);
-        when(randomNumber.generateBetweenOneAndInputNumber(maxCountCoursesOfStudent)).thenReturn(1, 2, 3);
-        when(randomNumber.generateBetweenOneAndInputNumber(countCourses)).thenReturn(4, 5, 6, 7, 8, 9);
+        when(dataConduct.getCourses()).thenReturn(courses);
+        when(dataConduct.getStudents()).thenReturn(students);
+        when(amountLimit.getMaxCountCoursesOfStudent()).thenReturn(maxCountCoursesOfStudent);
+        when(random.getInt(maxCountCoursesOfStudent)).thenReturn(2).thenReturn(1).thenReturn(3);
+        when(random.getLong(courses.size())).thenReturn(1L, 2L).thenReturn(1L).thenReturn(1L, 2L, 3L);
 
-        List<StudentAtCourse> studentAtCourses = studentToCourseGenerator.addStudentToCourse(students, countCourses);
+        List<StudentAtCourse> result = studentToCourseGenerator.generate();
+        
+        assertAll(
+        		() -> assertThat(result).hasSize(6),
+        		() -> assertThat(result).usingRecursiveComparison().isEqualTo(expect)
+        		);
 
-        for (StudentAtCourse studentAtCourse : studentAtCourses) {
-            Student student = studentAtCourse.getStudent();
-            Course course = studentAtCourse.getCourse();
-
-            assertThat(student.getStudentID() > 0).isTrue();
-            assertThat((course.getCourseID() > 0) && (course.getCourseID() <= countCourses)).isTrue();
-        }
-
-        verify(countConfig).getMaxCountCoursesOfStudent();
-        verify(randomNumber, times(3)).generateBetweenOneAndInputNumber(maxCountCoursesOfStudent);
-        verify(randomNumber, times(6)).generateBetweenOneAndInputNumber(countCourses);
+        verify(dataConduct).getCourses();
+        verify(dataConduct).getStudents();
+        verify(amountLimit).getMaxCountCoursesOfStudent();
+        verify(random, times(3)).getInt(courses.size());
+        verify(random, times(6)).getLong(maxCountCoursesOfStudent);
     }
 }
