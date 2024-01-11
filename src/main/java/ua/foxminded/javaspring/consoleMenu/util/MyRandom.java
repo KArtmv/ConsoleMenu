ea8da9f9d0551bbed0794a773d085cb52f069543
”Kyrylo <kirillartemov1993@gmail.com>
2024-01-11 17:03:00 +0100

    Fix to generate correct random number

--- src/main/java/ua/foxminded/javaspring/consoleMenu/util/MyRandom.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/util/MyRandom.java
@@ -5,10 +5,10 @@ import java.util.concurrent.ThreadLocalRandom;
 public class MyRandom {
 
     public int getInt(int bound){
-        return ThreadLocalRandom.current().nextInt(1, bound);
+        return ThreadLocalRandom.current().nextInt(bound);
     }
 
     public long getLong(int bound){
-        return ThreadLocalRandom.current().nextLong(1, bound);
+        return ThreadLocalRandom.current().nextLong(1, bound + 1);
     }
 }
