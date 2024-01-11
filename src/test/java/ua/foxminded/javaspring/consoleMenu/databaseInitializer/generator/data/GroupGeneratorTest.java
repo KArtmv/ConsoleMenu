4cf1e8f0f2abdc0b519b3b720c1ce2b82b9964b0
”Kyrylo <kirillartemov1993@gmail.com>
2024-01-11 17:19:37 +0100

    Change package

--- src/test/java/ua/foxminded/javaspring/consoleMenu/databaseInitializer/generator/data/GroupGeneratorTest.java
+++ src/test/java/ua/foxminded/javaspring/consoleMenu/databaseInitializer/generator/data/GroupGeneratorTest.java
@@ -1,4 +1,4 @@
-package ua.foxminded.javaspring.consoleMenu.data.generator;
+package ua.foxminded.javaspring.consoleMenu.databaseInitializer.generator.data;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
