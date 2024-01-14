d6ff52cbd3ba4c0788478b5da7d42e1ef842f8f6
”Kyrylo <kirillartemov1993@gmail.com>
2024-01-14 16:24:52 +0100

    Wrap adding course to Course class

--- src/main/java/ua/foxminded/javaspring/consoleMenu/rowmapper/StudentMapper.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/rowmapper/StudentMapper.java
@@ -1,6 +1,7 @@
 package ua.foxminded.javaspring.consoleMenu.rowmapper;
 
 import org.springframework.jdbc.core.RowMapper;
+import ua.foxminded.javaspring.consoleMenu.model.Group;
 import ua.foxminded.javaspring.consoleMenu.model.Student;
 
 import java.sql.ResultSet;
@@ -11,6 +12,6 @@ public class StudentMapper implements RowMapper<Student> {
     @Override
     public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
         return new Student(rs.getLong("student_id"), rs.getString("first_name"), rs.getString("last_name"),
-                rs.getString("group_name"));
+                new Group(rs.getString("group_name")));
     }
 }
