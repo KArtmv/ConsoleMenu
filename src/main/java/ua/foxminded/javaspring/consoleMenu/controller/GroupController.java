71a57ddbd390167f51a5c11189651846bf77ac50
”Kyrylo <kirillartemov1993@gmail.com>
2024-01-15 16:08:06 +0100

    Refactor getting nuber by InputHandler class instead MyConsole -Add more log events

--- src/main/java/ua/foxminded/javaspring/consoleMenu/controller/GroupController.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/controller/GroupController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
 import ua.foxminded.javaspring.consoleMenu.model.CounterStudentsAtGroup;
+import ua.foxminded.javaspring.consoleMenu.options.console.input.InputHandler;
 import ua.foxminded.javaspring.consoleMenu.options.console.input.MyScanner;
 import ua.foxminded.javaspring.consoleMenu.options.console.output.ConsolePrinter;
 import ua.foxminded.javaspring.consoleMenu.service.GroupService;
@@ -18,29 +19,33 @@ import java.util.List;
 public class GroupController {
     private static final Logger LOGGER = LoggerFactory.getLogger(GroupController.class);
     private GroupService groupService;
-    private MyScanner scanner;
+    private InputHandler inputHandler;
     private ConsolePrinter consolePrinter;
     private ApplicationMessages messages;
 
     @Autowired
-    public GroupController(GroupService groupService, MyScanner scanner, ConsolePrinter consolePrinter, ApplicationMessages messages) {
+    public GroupController(GroupService groupService, InputHandler inputHandler, ConsolePrinter consolePrinter, ApplicationMessages messages) {
         this.groupService = groupService;
-        this.scanner = scanner;
+        this.inputHandler = inputHandler;
         this.consolePrinter = consolePrinter;
         this.messages = messages;
     }
 
     public void counterStudentsAtGroups() {
+        LOGGER.info("Run method: counterStudentsAtGroups");
         try {
             consolePrinter.print(messages.GROUPS_BY_COUNT);
-            int requestedAmountOfStudent = scanner.getInt();
+            int requestedAmountOfStudent = inputHandler.getRequiredAmountOfStudents();
             List<CounterStudentsAtGroup> studentsAtGroups = groupService.counterStudentsAtGroups(requestedAmountOfStudent);
+            LOGGER.debug("Received request to find less or equals amount of students by Groups: {}", requestedAmountOfStudent);
 
             if (CollectionUtils.isEmpty(studentsAtGroups)) {
                 consolePrinter.print(String.format(messages.HAS_NOT_GROUPS_WITH_COUNT , requestedAmountOfStudent));
             } else {
                 studentsAtGroups.sort((a, b) -> Long.compare(b.getStudentsCount(), a.getStudentsCount()));
                 consolePrinter.viewAmountStudentAtGroup(studentsAtGroups);
+
+                LOGGER.debug("Successfully received amount of students by groups.");
             }
         } catch (InputMismatchException e) {
             LOGGER.error("Error getting numbers: {}", e.getMessage());
