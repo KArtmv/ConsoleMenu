8c94e4d6010dbce2605d2d3ccb4539b5866e9761
”Kyrylo <kirillartemov1993@gmail.com>
2024-01-16 10:48:09 +0100

    Create tests

---
+++ src/test/java/ua/foxminded/javaspring/consoleMenu/databaseInitializer/generator/DataConductTest.java
@@ -0,0 +1,43 @@
+package ua.foxminded.javaspring.consoleMenu.databaseInitializer.generator;
+
+import org.junit.jupiter.api.Test;
+import ua.foxminded.javaspring.consoleMenu.model.Course;
+import ua.foxminded.javaspring.consoleMenu.model.Group;
+import ua.foxminded.javaspring.consoleMenu.model.Student;
+import ua.foxminded.javaspring.consoleMenu.pattern.InitializeObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataConductTest {
+
+    public static final InitializeObject INITIALIZE_OBJECT = new InitializeObject();
+    public static final DataConduct DATA_CONDUCT = new DataConduct();
+
+    @Test
+    void getStudents_shouldReturnListOfStudent_whenIsSet() {
+        List<Student> students = INITIALIZE_OBJECT.studentsListInit();
+        DATA_CONDUCT.setStudents(students);
+
+        assertThat(DATA_CONDUCT.getStudents()).isEqualTo(students);
+    }
+
+    @Test
+    void getCourses_shouldReturnListOfCourses_whenIsSet() {
+        List<Course> courses = INITIALIZE_OBJECT.coursesListInit();
+        DATA_CONDUCT.setCourses(courses);
+
+        assertThat(DATA_CONDUCT.getCourses()).isEqualTo(courses);
+    }
+
+    @Test
+    void getGroups() {
+        List<Group> groups = INITIALIZE_OBJECT.groupsListInit();
+        DATA_CONDUCT.setGroups(groups);
+
+        assertThat(DATA_CONDUCT.getGroups()).isEqualTo(groups);
+    }
+}
